ba1bb4777bbc01272f231590ab32323861c75673
Min <huminkng@gmail.com>
2016-06-27 17:50:19 +0000

    learnyoumongo 4 + added gitignore to ignore database

--- learnyoumongo/find-project.js
+++ learnyoumongo/find-project.js
@@ -3,3 +3,45 @@
 // find all documents where age is greater than first argument
 // we only want the name and age
 // print out
+var mongo = require("mongodb").MongoClient
+var url = "mongodb://localhost:27017/learnyoumongo"
+mongo.connect()
+mongo.connect(url,function(err,db){
+    var col = db.collection("parrots")
+    col.find({
+        age: {$gt:+process.argv[2]}
+        
+    }, {
+        _id:0
+        
+    }).toArray(function(err,documents){
+        console.log(documents)
+        db.close()
+    })
+})
+
+// official solution
+/*
+    var mongo = require('mongodb').MongoClient
+    var age = process.argv[2]
+    
+    var url = 'mongodb://localhost:27017/learnyoumongo'
+    
+    mongo.connect(url, function(err, db) {
+      if (err) throw err
+      var parrots = db.collection('parrots')
+      parrots.find({
+        age: {
+          $gt: +age
+        }
+      }, {
+        name: 1
+      , age: 1
+      , _id: 0
+      }).toArray(function(err, docs) {
+        if (err) throw err
+        console.log(docs)
+        db.close()
+      })
+    })
+*/
